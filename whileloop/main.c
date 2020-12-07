802ab7627ad6ad52cdee9bd1559cc93183a91406
omid-joukar <joukaromid6@gmail.com>
2020-12-07 11:06:26 +0100

    first commit

---
+++ whileloop/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void) 
+{
+printf("Dieses Programm gibt die Zahlen von 14 bis 23 in aufsteigender Reihenfolge aus\n\n");
+int i = 14;
+while(i <24)
+{
+  printf("%d \n",i);
+  i+=1;
+}
+  return 0;
+}
